6044d5b4ad350af5fbbe8041d1bde7b1a7b84475
Evelyn Koo <r_evelynkoo@berkeley.edu>
2023-11-18 11:41:34 -0800

    fix: outage vrect to 2023

--- pages/hourly.py
+++ pages/hourly.py
@@ -31,7 +31,7 @@ fig = go.Figure([trace])
 fig.add_hline(y=avg_change, line_color="gray", annotation_text=f"Avg Hourly Change: {np.round(avg_change, -2) / 10 ** 3}k")
 
 # outages
-fig.add_vrect(x0="2022-07-14 19:00", x1="2022-07-16 06:00", fillcolor="red", opacity=0.2, line_width=0)
+fig.add_vrect(x0="2023-11-17 02:00", x1="2023-11-18 17:37", fillcolor="red", opacity=0.2, line_width=0)
 
 # layout
 fig.update_xaxes(range=[tracker.START_DATE, utils.get_day()], rangeslider_visible=True,
