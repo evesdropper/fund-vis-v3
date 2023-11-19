dbcafe0975d1028dced193c9f17b6e443444735b
Evelyn Koo <r_evelynkoo@berkeley.edu>
2023-11-19 12:46:50 -0800

    fix: analytics error handling

--- pages/Analytics.py
+++ pages/Analytics.py
@@ -41,42 +41,60 @@ with col1:
     st.metric(label="Current Fund", value=f"{cfund}M", delta=f"{(unique_funds.iloc[-1, 1] - unique_funds.iloc[-2, 1]) / 10 ** 3}K")
 
 with col2:
-    st.metric(label="Change in Past 24 Hours", value=f"{inc_24}K", delta=f"{inc_24_pct}%")
+    try:
+        st.metric(label="Change in Past 24 Hours", value=f"{inc_24}K", delta=f"{inc_24_pct}%")
+    except:
+        st.metric(label="Change in Past 24 Hours", value="N/A", delta="N/A")
 
 with col3:
-    st.metric(label="Estimated Final Fund", value=f"{final_pred}M")
+    if final_pred > max(tracker.CHECKPOINTS.keys()) * 3:
+        final_pred_value = "N/A"
+    else: 
+        final_pred_value = f"{final_pred}M"
+    st.metric(label="Estimated Final Fund", value=final_pred_value)
 
 st.header("Checkpoint Info")
 
-if cfund < 15:
+# get checkpoint information
+checknums = list(tracker.CHECKPOINTS.keys())
+idx = 0
+while checknums[idx] < cfund:
+    idx += 1
+next_checkpoint = tracker.CHECKPOINTS[checknums[idx]]
+
+if cfund < max(tracker.CHECKPOINTS.keys()):
     cdelta = tracker.tdelta_format(mdates.num2date(tracker.newton() + tracker.X_SHIFT).replace(tzinfo=datetime.timezone.utc) - datetime.datetime.now().replace(tzinfo=datetime.timezone.utc))
 else:
     cdelta = "N/A"
-    
+
 col1, col2, col3 = st.columns(3)
 with col1:
-    st.metric(label="Next Checkpoint", value=f"{tracker.CHECKPOINTS.get(np.ceil(df.iloc[-1, 1] / 10 ** 6), 'N/A')}")
+    st.metric(label="Next Checkpoint", value=next_checkpoint)
 
 with col2:
     st.metric(label="Est. Time To Reach", value=f"{cdelta}")
 
 with col3:
-    st.metric(label="Est. End Checkpoint", value=f"{tracker.CHECKPOINTS[min(np.floor(final_pred), 15)]}")
-
+    if final_pred > max(tracker.CHECKPOINTS.keys()) * 3:
+        final_checkpoint = "N/A"
+    else:
+        final_checkpoint = f"{tracker.CHECKPOINTS[min(np.floor(final_pred), max(tracker.CHECKPOINTS.keys()))]}"
+    st.metric(label="Est. End Checkpoint", value=final_checkpoint)
 
 st.header("Fund Daily Changes")
 
-
-daily = df["Time"].str.extract(rf'(\s2:0[012]|\s1:5[789])').dropna()
-
+try:
+# get daily
+    daily = df["Time"].str.extract(rf'(\s2:0[012]|\s1:5[789])').dropna()
 # # draw time series
-df_d = df.iloc[daily.index]
-df_d["Day"] = list(range(1, df_d.shape[0] + 1))
-df_d["Diff"] = np.round(df_d["Fund"].diff(), -1).fillna(0).astype(int)
-df_d.loc[55, "Diff"] = df_d.loc[55, "Fund"].astype(int)
-df_d["% Change"] = np.round(df_d["Diff"].pct_change().fillna(0) * 100, 3)
-df_d["% Change"] = df_d["% Change"].apply(str)
-df_d["% Change"] = df_d["% Change"].apply(utils.format_percent)
-daily_final = df_d[["Day", "Diff", "% Change"]].set_index("Day")
-
-st.dataframe(data=daily_final)
+    df_d = df.iloc[daily.index]
+    df_d["Day"] = list(range(1, df_d.shape[0] + 1))
+    df_d["Diff"] = np.round(df_d["Fund"].diff(), -1).fillna(0).astype(int)
+    df_d.loc[55, "Diff"] = df_d.loc[55, "Fund"].astype(int)
+    df_d["% Change"] = np.round(df_d["Diff"].pct_change().fillna(0) * 100, 3)
+    df_d["% Change"] = df_d["% Change"].apply(str)
+    df_d["% Change"] = df_d["% Change"].apply(utils.format_percent)
+    daily_final = df_d[["Day", "Diff", "% Change"]].set_index("Day")
+    st.dataframe(data=daily_final)
+except:
+    pass
